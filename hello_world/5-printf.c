eb15026f013169482ba32cb2b913a6457c49de91
nchenozs <nxenozxs@gmail.com>
2023-10-21 13:54:19 -0700

    task 5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using the printf function
+ * main - print the string in the put function
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle
  * Return: 0
- */
-int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
-}
+ * */
 
+int main(void)
+
+{
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
