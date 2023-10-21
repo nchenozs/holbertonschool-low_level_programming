b33501b6b09a5607b8b6d4d5ad47b31afbd358e3
nchenozs <nxenozxs@gmail.com>
2023-10-21 14:41:13 -0700

    task 6

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -8,13 +8,10 @@
  */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-
-{
+	for (c = '0'; c <= '9'; c++)
 	putchar(c);
-}
-putchar('\n');
-return (0);
+	putchar('\n');
+
+	return (0);
 }
 
